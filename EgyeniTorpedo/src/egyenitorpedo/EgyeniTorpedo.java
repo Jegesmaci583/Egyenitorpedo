cf363f2388847e1a1fbcf89a53826cfc09bb5001
Jegesmaci583 <80511513+Jegesmaci583@users.noreply.github.com>
2022-05-19 15:46:40 +0200

    Github oldalról feltöltve
    Valahol az elején már elvesztettem a fonalat meg a git se azt akarta amit én de egy plusz 1es így is úgy is lesz :3

---
+++ EgyeniTorpedo/src/egyenitorpedo/EgyeniTorpedo.java
@@ -0,0 +1,9 @@
+package egyenitorpedo;
+public class EgyeniTorpedo {
+    public static void main(String[] args) {
+     String t = "talált";
+     assert t.equals("Talált") : "Nem jó!";
+     Hajo.talalat();
+     
+    }    
+}
---
+++ EgyeniTorpedo/src/egyenitorpedo/Hajo.java
@@ -0,0 +1,8 @@
+package egyenitorpedo;
+public class Hajo {
+    private int[] pozicio = new int [3];
+    
+    public String talalat(int poz) {
+        ;
+    }
+}
